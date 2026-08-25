7e7dd7e8305be032c01a4547bc8b575f56c0f30b
3chapin <46701179+3chapin@users.noreply.github.com>
2026-08-25 17:46:38 -0600

    changed some things

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+const savedTheme = localStorage.getItem('theme')
+
+if (savedTheme === 'dark' || savedTheme === 'light') {
+	document
+		.getElementById('tiny-theme')
+		?.setAttribute('href', `/tiny/${savedTheme}.css`)
+}
+
+const setTheme = (theme: 'light' | 'dark') => {
+	document
+		.getElementById('tiny-theme')
+		?.setAttribute('href', `/tiny/${theme}.css`)
+
+	localStorage.setItem('theme', theme)
+}
 </script>
 
 <template>
-  <HelloWorld />
+	<div class="flex flex-col items-center gap-y-4">
+		<h1>hello world</h1>
+		<button @click="setTheme('light')">light</button>
+		<button @click="setTheme('dark')">dark</button>
+	</div>
 </template>
